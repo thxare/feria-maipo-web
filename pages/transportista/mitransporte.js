df870dee11d43d37e744661c0740200e008aea33
Thiare Saez <thiareayleen@gmail.com>
2022-09-29 21:52:08 -0300

    Solucion mitransporte

--- pages/transportista/mitransporte.js
+++ pages/transportista/mitransporte.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Header from "../../components/header/Header";
+import { Header } from "../../components/header/Header";
 
 export default function MiTransporte() {
   const funciones = [
