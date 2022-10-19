99f9be669fecaf023f6d22a85bf8c81ca3629cc3
Thiare <thiareayleen@gmail.com>
2022-10-19 18:40:22 -0300

    get con api peticion

--- components/productor/Ventas.js
+++ components/productor/Ventas.js
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { ModalParticiparBackdrop } from "./venta/ModalParticiparBackdrop";
 import { SolicitudCard } from "./venta/SolicitudCard";
 
 export const Ventas = ({ tipo }) => {
   const [show, setShow] = useState(false);
+  const [peticion, setPeticion] = useState({});
   const handleClick = () => {
     setShow(!show);
   };
-  
+
+  const url = "https://api-feria-web-production.up.railway.app/api/peticion";
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setPeticion(res.data);
+    });
+  }, []);
+
+
   return (
     <div className="mx-auto mt-8 h-fit w-10/12">
       <div className="mx-3 flex flex-col justify-between sm:mx-0 sm:flex-row">
@@ -15,7 +26,16 @@ export const Ventas = ({ tipo }) => {
       </div>
       {show && <ModalParticiparBackdrop closeModal={handleClick} />}
       <div className="grid grid-cols-3">
-        <SolicitudCard handleClick={handleClick} />
+        {peticion.map((pet) => {
+          return (
+            <SolicitudCard
+              handleClick={handleClick}
+              nombre={pet.nombre}
+              kilogramos={pet.kilogramos}
+              estado={pet.estado}
+            />
+          );
+        })}
       </div>
     </div>
   );
