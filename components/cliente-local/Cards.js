5777b08c200f51fa6aa0180650430f40547bac49
Thiare <thiareayleen@gmail.com>
2022-11-09 20:15:49 -0300

    componente card en cliente local saldos

--- components/cliente-local/Cards.js
+++ components/cliente-local/Cards.js
@@ -1,20 +1,17 @@
 import React from "react";
 
-import { Card } from "./Card";
+import { CardCliente } from "./CardCliente";
 export const Cards = () => {
   return (
-    <div className="h-max bg-gray-100 ">
-      <h1 className="mb-2 p-10 text-3xl font-semibold">Saldos</h1>
-      <div className="container mx-auto flex min-h-screen justify-center">
-        <div className="mx-auto grid  w-full grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-5">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+    <>
+      <div className="mx-auto flex justify-center">
+        <div className="grid pb-4 sm:grid-cols-2 sm:gap-4 md:grid-cols-3 lg:grid-cols-4">
+          <CardCliente />
+          <CardCliente />
+          <CardCliente />
+          <CardCliente />
         </div>
       </div>
-    </div>
+    </>
   );
 };
