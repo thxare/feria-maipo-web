3abc0fbf617571bfde366ed26cbab92391e1fe12
Thiare <thiareayleen@gmail.com>
2022-11-09 20:18:46 -0300

    se elimina el componente card de cliente local

--- components/cliente-local/Cards.js
+++ components/cliente-local/Cards.js
@@ -1,15 +1,36 @@
 import React from "react";
+import { Card } from "../ui/Card";
+import { Button } from "../ui/Button";
+import { ValoresCard } from "../ui/ValoresCard";
 
-import { CardCliente } from "./CardCliente";
 export const Cards = () => {
   return (
     <>
       <div className="mx-auto flex justify-center">
         <div className="grid pb-4 sm:grid-cols-2 sm:gap-4 md:grid-cols-3 lg:grid-cols-4">
-          <CardCliente />
-          <CardCliente />
-          <CardCliente />
-          <CardCliente />
+          <Card
+            nombreCard={"Mango"}
+            img={"/uva.png"}
+            button={
+              <>
+                <Button
+                  text={"Comprar Producto"}
+                  colorBtn={"bg-green"}
+                  hoverBtn={"hover:bg-darkGreen"}
+                />
+              </>
+            }
+            valores={
+              <>
+                <ValoresCard nombre={"Precio"} valor={"$6.990 x kg"} />
+                <ValoresCard nombre={"Calidad"} valor={"Extra"} />
+                <ValoresCard
+                  nombre={"Observaciones"}
+                  valor={"Excelente fruta"}
+                />
+              </>
+            }
+          />
         </div>
       </div>
     </>
