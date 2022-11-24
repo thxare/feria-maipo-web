9535a920e170d42931481434bef2b140da1b4101
AlenGuerrero <alei.guerrero@duocuc.cl>
2022-11-24 09:50:49 -0300

    input terminado

--- components/cliente-local/Cards.js
+++ components/cliente-local/Cards.js
@@ -4,31 +4,37 @@ import { Button } from "../ui/Button";
 import { ValoresCard } from "../ui/ValoresCard";
 import { CardContainer } from "../ui/CardContainer";
 
-export const Cards = () => {
+export const Cards = ({productos}) => {
   return (
     <>
       <CardContainer>
-        {/* Esto luego ir dentro de un map, y los valores se deben traer del servidor */}
-        <Card
-          nombreCard={"Mango"}
-          img={"/uva.png"}
-          button={
-            <>
-              <Button
-                text={"Comprar Producto"}
-                colorBtn={"bg-green"}
-                hoverBtn={"hover:bg-darkGreen"}
-              />
-            </>
-          }
-          valores={
-            <>
-              <ValoresCard nombre={"Precio"} valor={"$6.990 x kg"} />
-              <ValoresCard nombre={"Calidad"} valor={"Extra"} />
-              <ValoresCard nombre={"Observaciones"} valor={"Excelente fruta"} />
-            </>
-          }
-        />
+      {productos.map((products) => {
+          return (
+            <Card
+              nombreCard={products.nombre}
+              precio={products.precio}
+              cantidad={products.cantidad}
+              img={products.imagen}
+              button={
+                <>
+                  <Button
+                    text={"Agregar"}
+                    colorBtn={"bg-green"}
+                    hoverBtn={"hover:bg-darkGreen"}
+                  />
+                </>
+              }
+              valores={
+                <>
+                  <ValoresCard
+                    nombre={"Observaciones"}
+                    valor={products.observaciones}
+                  />
+                </>
+              }
+            />
+          );
+        })}
       </CardContainer>
     </>
   );
--- pages/cliente-local/index.js
+++ pages/cliente-local/index.js
@@ -1,15 +1,16 @@
 import Head from "next/head";
 import { router } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Header } from "../../components/header/Header";
 import { Cards } from "../../components/cliente-local/Cards";
 import { ContainerPage } from "../../components/ui/ContainerPage";
+import axios from "axios";
 
 export default function Index() {
   const funciones = [
-    { name: "Mercado", link: "/cliente-local/" },
-    { name: "Intancias de Compra", link: "/cliente-local/Intancias" },
-    { name: "Solicitudes", link: "/cliente-local/solicitudes" },
+    { name: "Saldos", link: "/cliente-local/" },
+    { name: "Productos", link: "/cliente-local/Intancias" },
+    /* { name: "Solicitudes", link: "/cliente-local/solicitudes" }, */
   ];
   useEffect(() => {
     const dato = JSON.parse(localStorage.getItem("loggedNoteAppUser"));
@@ -19,6 +20,22 @@ export default function Index() {
       router?.push("/cliente-local/");
     }
   }, []);
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    const getProductos = async () => {
+      const res = await axios.get(
+        "https://api-feria-web-production.up.railway.app/api/productos/"
+      );
+      const datos = res.data;
+      const filteredDatos = datos.filter(
+        (x) =>
+          x.saldo === 1 
+      );
+      setProductos(filteredDatos);
+    };
+    getProductos();
+  }, [productos]);
 
   return (
     <>
@@ -28,7 +45,7 @@ export default function Index() {
       <div>
         <Header funciones={funciones} />
         <ContainerPage titulo={"Saldos"}>
-          <Cards />
+          <Cards productos={productos}/>
         </ContainerPage>
       </div>
     </>
